ffba5473fac8c895e72012ddddb0a1a441d840ed
finke <Surf-finke@gmx.de>
2011-11-08 11:59:25 +0100

    Update File hinzugefuegt

---
+++ updates/update_0975.php
@@ -0,0 +1,24 @@
+<?php
+  include ("../inc.conf.php");
+  include ("../inhalt/inc.hilfsfunktionen.php");
+  
+  $conn = @mysql_connect($server.':'.$port,"$login","$password");
+  $db = @mysql_select_db("$database",$conn);
+  $zeiger = @mysql_query("SELECT version FROM $skrupel_info");
+  $array = @mysql_fetch_array($zeiger);
+  $spiel_version = $array["version"];
+  if ($spiel_version=='0.974') {
+    @mysql_query("UPDATE `$skrupel_info` set version='0.975'");
+	@mysql_query("ALTER TABLE `$skrupel_user` ADD COLUMN `salt` varchar(16) NOT NULL default '' AFTER `passwort`");
+	$user = @mysql_query("SELECT `id`, `passwort`,  FROM `$skrupel_user`");
+	while($id = mysql_fetch_array($user)){
+			$passwd=cryptPasswd($id['passwort']);
+			$passwd= explode(':',$passwd, 2);
+            $zeiger_temp = @mysql_query("UPDATE `$skrupel_user` set passwort='{$passwd[0]}', salt = '{$passwd[1]}' where id={$id['id']}");
+	}
+    echo "Die Datenbankstruktur wurde erfolgreich aktualisiert.";
+  } else {
+   echo "<b>Fehler</b>: Dieser Patch ben&ouml;tigt Version 0.974, die aktuelle Version ist aber $spiel_version.";
+  }
+  @mysql_close();
+
